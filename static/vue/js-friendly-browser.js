6462b2931b3783977a3e83388bff812b2f960d06
healingdrawing <healingdrawing@gmail.com>
2024-08-15 01:13:15 +0300

    remove body background image if javascript is allowed

---
+++ static/vue/js-friendly-browser.js
@@ -0,0 +1,6 @@
+/*
+  remove static background in case of javascript is allowed,
+  otherwise the ghost is present sometimes when slideshow transition executed.
+*/
+document.body.style.backgroundImage = "none";
+document.body.style.backgroundColor = "#000";
