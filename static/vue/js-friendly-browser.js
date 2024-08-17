0a85d813306a8c1113a4da2e05988f01fe6ad535
healingdrawing <healingdrawing@gmail.com>
2024-08-17 23:36:45 +0300

    patch to prevent background blinking using delay. was: static -> black background -> slideshow. now: static -> slideshow -> black background

--- static/vue/js-friendly-browser.js
+++ static/vue/js-friendly-browser.js
@@ -1,6 +1,10 @@
 /*
-  remove static background in case of javascript is allowed,
+  remove static background
+  (with delay to prevent blinking: static -> black -> dynamic, detected by web analyzers)
+  in case of javascript is allowed,
   otherwise the ghost is present sometimes when slideshow transition executed.
 */
-document.body.style.backgroundImage = "none";
-document.body.style.backgroundColor = "#000";
+setTimeout(function() {
+  document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "#000";
+}, 4000);
